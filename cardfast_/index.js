e80a06ff2e1b515cf2bcb5274a24ad286d9bb183
Erick S. Gaspar <esgaspar@msn.com>
2019-04-14 21:41:02 -0300

    t

--- cardfast_/index.js
+++ cardfast_/index.js
@@ -1,5 +1,5 @@
 var app = require('./config/custom-express')();
 
 app.listen(3001, function(){
-  console.log('Servidor de cartoes rodando na porta 3001.');
+  console.log('Servidor de cartoes rodando na porta 3001. ');
 });
